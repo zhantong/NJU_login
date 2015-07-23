c945a00303bce01d70d2ad45d891e3cfffa9d5aa
zhantong1994 <zhantong1994@163.com>
2015-07-23 20:36:15 +0800

    修改账号密码界面优化，增加关于界面
    修改账号密码界面直接采用简单的网格布局，增加了关于作者界面，修复了主窗体上“关闭程序”的default属性

--- window.py
+++ window.py
@@ -4,7 +4,7 @@ import nju_login_2
 class ChangeInfo(QDialog):
     def __init__(self, parent=None):
         super(ChangeInfo, self).__init__(parent)
-        usr = QLabel("用户：")
+        usr = QLabel("账号：")
         pwd = QLabel("密码：")
         self.usrLineEdit = QLineEdit()
         self.pwdLineEdit = QLineEdit()
@@ -13,33 +13,28 @@ class ChangeInfo(QDialog):
         gridLayout = QGridLayout()
         gridLayout.addWidget(usr, 0, 0, 1, 1)
         gridLayout.addWidget(pwd, 1, 0, 1, 1)
-        gridLayout.addWidget(self.usrLineEdit, 0, 1, 1, 3);
-        gridLayout.addWidget(self.pwdLineEdit, 1, 1, 1, 3);
+        gridLayout.addWidget(self.usrLineEdit, 0, 1, 1, 2);
+        gridLayout.addWidget(self.pwdLineEdit, 1, 1, 1, 2);
 
         okBtn = QPushButton("确定")
         cancelBtn = QPushButton("取消")
-        btnLayout = QHBoxLayout()
-
-        btnLayout.setSpacing(60)
-        btnLayout.addWidget(okBtn)
-        btnLayout.addWidget(cancelBtn)
-
-        dlgLayout = QVBoxLayout()
-        dlgLayout.setContentsMargins(40, 40, 40, 40)
-        dlgLayout.addLayout(gridLayout)
-        dlgLayout.addStretch(40)
-        dlgLayout.addLayout(btnLayout)
-
-        self.setLayout(dlgLayout)
+        gridLayout.addWidget(okBtn, 2, 0, 1, 1)
+        gridLayout.addWidget(cancelBtn, 2, 1, 1, 1)
+        aboutBtn=QPushButton("关于作者")
+        gridLayout.addWidget(aboutBtn, 2, 2, 1, 1)
+        self.setLayout(gridLayout)
         okBtn.clicked.connect(self.ok)
+        aboutBtn.clicked.connect(self.about)
         cancelBtn.clicked.connect(self.reject)
-        self.setWindowTitle("登录")
-        self.resize(300, 200)
+        self.setWindowTitle("修改账号密码")
+        #self.resize(300, 200)
         self.usrLineEdit.setText(nju.id)
         self.pwdLineEdit.setText(nju.pw)
     def ok(self):
         nju.set_id_pw(self.usrLineEdit.text(),self.pwdLineEdit.text())
         super(ChangeInfo, self).accept()
+    def about(self):
+        QMessageBox.about(self,"关于作者","如果你有任何意见或建议\n欢迎联系我：zhantong1994@163.com")
 class LoginDlg(QWidget):
     def __init__(self, parent=None):
         super(LoginDlg, self).__init__(parent)
@@ -51,14 +46,17 @@ class LoginDlg(QWidget):
         gridLayout.addWidget(self.conBtn, 1, 0, 1, 1)
         gridLayout.addWidget(changeinfoBtn, 1, 1, 1, 1)
         cancelBtn=QPushButton("关闭程序")
+        cancelBtn.setDefault(True)
         gridLayout.addWidget(cancelBtn, 2, 0,1,2)
         self.setLayout(gridLayout)
+        self.setWindowTitle("南京大学|校园网自动登录")
         cancelBtn.setFocus()
         cancelBtn.clicked.connect(self.close)
         changeinfoBtn.clicked.connect(self.show_info)
         self.conBtn.clicked.connect(self.connect)
         self.browser.setReadOnly(True)
         self.conBtn.clicked.emit(True)
+
     def show_info(self):
         cinfo=ChangeInfo()
         cinfo.show()
@@ -72,6 +70,7 @@ class LoginDlg(QWidget):
             self.conBtn.setText('连接')
         else:
             self.conBtn.setText('断开')
+
 if __name__=='__main__':
     nju=nju_login_2.NJU_Login()
     app = QApplication(sys.argv)
